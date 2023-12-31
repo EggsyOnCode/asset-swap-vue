30e458656cccb51c824c12afbf7cfaffe2b3a4df
EggsyOnCode <farzansaqib2003@gmail.com>
2023-12-31 23:43:35 +0500

    added OrderCard List

--- src/components/Orders/OrderCard.vue
+++ src/components/Orders/OrderCard.vue
@@ -1,21 +1,26 @@
 <template lang="html">
-  <div class="w-7/12 flex flex-row bg-primary rounded-lg">
+  <div class="w-7/12 flex flex-row bg-secondary rounded-lg my-2">
     <img
       src="../Orders/../../assets/wallpaper.jpg"
       alt="free-img-api"
-      class="p-3 w-1/3 rounded-[11px]"
+      class="w-1/3 m-3 rounded-lg"
     />
-    <div class="flex flex-col items-start justify-around w-1/3">
+    <div
+      class="flex flex-col items-start justify-around w-1/3 text-black text-xl"
+    >
       <h1>{{ name }}</h1>
       <h1>{{ supplier }}</h1>
-      <h1>{{ price }}</h1>
+      <v-chip elevated color="black" theme="light" class="bg-accent rounded-xl">
+        <v-icon icon="mdi-cash"></v-icon>
+        {{ price }}
+      </v-chip>
     </div>
     <div class="flex flex-col justify-center items-center w-1/3">
-      <button class="py-1 px-4 rounded-[11px] bg-green flex-shrink-0">
+      <button class="py-1 px-4 rounded-[11px] bg-green flex-shrink-0 font-bold">
         Inspect
       </button>
       <h1 class="m-3">Or</h1>
-      <button class="py-1 px-4 rounded-[11px] bg-green flex-shrink-0">
+      <button class="py-1 px-4 rounded-[11px] bg-green flex-shrink-0 font-bold">
         Complete
       </button>
     </div>
---
+++ src/components/Orders/OrderList.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+  <div class="mt-6 flex flex-col items-center">
+    <OrderCardVue :name="'Ferrari'" :supplier="'Toyota'" :price="'98.89'" />
+    <OrderCardVue :name="'Ferrari'" :supplier="'Toyota'" :price="'98.89'" />
+    <OrderCardVue :name="'Ferrari'" :supplier="'Toyota'" :price="'98.89'" />
+  </div>
+</template>
+<script lang="ts">
+import OrderCardVue from "./OrderCard.vue";
+
+export default {
+  components: {
+    OrderCardVue,
+  },
+};
+</script>
+<style lang=""></style>
