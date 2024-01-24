fdb8090fc24b515d41500d43b555a22d86ae6686
EggsyOnCode <farzansaqib2003@gmail.com>
2024-01-24 20:43:59 +0400

    added class mehtods for contract interactions

--- src/utils/contractInteraction.ts
+++ src/utils/contractInteraction.ts
@@ -19,16 +19,30 @@ export async function deployOrderManagerContract(
     orderManagerAbi,
     rpcProvider
   );
-  const orderManager = new OrderManager(contract.address, contractRef);
+  const orderManager = new OrderManager(contract.address, contractRef, price);
   return orderManager;
 }
 
 class OrderManager {
   constructor(
     public contractAddress: string,
-    private contractRef: ethers.Contract
+    private contractRef: ethers.Contract,
+    private price: string
   ) {}
-  
+
+  async getBalance() {
+    const balance = this.contractRef.getBalance();
+    return balance / 1e18;
+  }
+
+  async deposit() {
+    const options = { value: ethers.utils.parseEther(this.price) };
+    await this.contractRef.deposit(options);
+  }
+
+  async withdraw() {
+    await this.contractRef.withdraw();
+  }
 }
 
 export async function deployCarNftContract(
@@ -43,13 +57,30 @@ export async function deployCarNftContract(
     carNftABI,
     rpcProvider
   );
-  const carNft = new CarNFT(contract.address, contractRef);
+  const carNft = new CarNFT(contract.address, contractRef, price);
   return carNft;
 }
 
 class CarNFT {
   constructor(
     public contractAddress: string,
-    private contractRef: ethers.Contract
+    private contractRef: ethers.Contract,
+    private price: string
   ) {}
+
+  async getOwner() {
+    await this.contractRef.getOwner();
+  }
+
+  async resetOwner(address: string) {
+    await this.contractRef.resetOwner(address);
+  }
+  // tokenUri is the ipfs store of the details of the asset
+  async mint(to: string, tokenUri: string) {
+    await this.contractRef.mint(to, tokenUri);
+  }
+
+  async tokenTransfer(from: string, to: string, tokenId: number) {
+    await this.contractRef.tokenTransfer(from, to, tokenId);
+  }
 }
