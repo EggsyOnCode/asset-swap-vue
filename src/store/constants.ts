0bfd810e2b88c9395d901d9f703acd44f312f328
EggsyOnCode <farzansaqib2003@gmail.com>
2024-01-29 20:22:37 +0400

    added conditional rendering based onthe buying  chip states and selections

---
+++ src/store/constants.ts
@@ -0,0 +1,5 @@
+export enum StatesOrder {
+  REQUESTED = "requested",
+  INSPECTED = "inspected",
+  COMPLETED = "completed",
+}
--- src/views/BuyingOrders.vue
+++ src/views/BuyingOrders.vue
@@ -1,21 +1,56 @@
 <template lang="html">
   <div class="flex flex-col items-center mt-10 font-garamond">
     <SearchBarVue />
-    <ChipStatesVue />
-    <BuyingOrderListVue />
+    <div class="w-8/12 flex flex-row justify-around mt-6">
+      <ChipVue
+        :color="'bg-primary'"
+        @click="setRequested"
+        :text="'Requested'"
+      />
+      <ChipVue
+        :color="'bg-secondary'"
+        :text="'Inspection'"
+        @click="setInspected"
+      />
+      <ChipVue :color="'bg-green'" :text="'Completed'" @click="setCompleted" />
+    </div>
+    <BuyingOrderListRequestedVue v-if="chipState == 'requested'" />
+    <BuyingOrdersListInspectedVue v-if="chipState == 'inspected'" />
+    <BuyingOrdersListCompletedVue v-if="chipState == 'completed'" />
   </div>
 </template>
 <script lang="ts">
 import SearchBarVue from "@/components/Orders/SearchBar.vue";
-import ChipStatesVue from "@/components/Orders/ChipStates.vue";
-import BuyingOrderListVue from "@/components/Orders/BuyingOrderList.vue";
-
-export default {
+import { StatesOrder } from "@/store/constants";
+import { defineComponent } from "vue";
+import ChipVue from "@/components/Orders/Chip.vue";
+import BuyingOrderListRequestedVue from "@/components/Orders/BuyingOrderListRequested.vue";
+import BuyingOrdersListInspectedVue from "@/components/Orders/BuyingOrdersListInspected.vue";
+import BuyingOrdersListCompletedVue from "@/components/Orders/BuyingOrdersListCompleted.vue";
+export default defineComponent({
   components: {
     SearchBarVue,
-    ChipStatesVue,
-    BuyingOrderListVue,
+    ChipVue,
+    BuyingOrderListRequestedVue,
+    BuyingOrdersListInspectedVue,
+    BuyingOrdersListCompletedVue,
   },
-};
+  data() {
+    return {
+      chipState: StatesOrder.REQUESTED,
+    };
+  },
+  methods: {
+    setRequested() {
+      this.chipState = StatesOrder.REQUESTED;
+    },
+    setInspected() {
+      this.chipState = StatesOrder.INSPECTED;
+    },
+    setCompleted() {
+      this.chipState = StatesOrder.COMPLETED;
+    },
+  },
+});
 </script>
 <style lang=""></style>
